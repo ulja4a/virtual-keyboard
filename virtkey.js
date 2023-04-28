6730561f1e1d026a65c109658d74c9f0157cc2e2
ulja4a <ulja4a@gmail.com>
2023-04-28 19:38:22 +0400

    feat: add textarea and css for it

--- virtkey.js
+++ virtkey.js
@@ -1,12 +1,17 @@
 const title = document.createElement("h1");
-
+const textarea = document.createElement("textarea");
 
 // Добавление класса
 document.body.classList.add("body");
 title.classList.add("title");
+textarea.classList.add("textarea");
 
-// Установка текстового содержимого
+// Установка св-в и текстового содержимого
 title.textContent = "RSS Virtual keyboard";
+textarea.rows = 5;
+textarea.cols = 50;
+textarea.placeholder = 'Enter your text ...'
 
 // Добавление элемента на страницу
 document.body.appendChild(title);
+document.body.appendChild(textarea);
